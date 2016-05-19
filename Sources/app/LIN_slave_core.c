192eb511b669fb550c13726748a51a11e1f22d51
adrfrank <adrfrank@outlook.com>
2016-05-19 03:41:19 -0500

    Maquina de estados implementada

--- Sources/app/LIN_slave_core.c
+++ Sources/app/LIN_slave_core.c
@@ -82,6 +82,8 @@ void linslv_slaveStateMachine(E_LINSLV_COMM_EVENTS le_event)
 	static T_UBYTE lub_tempChksum; /* Holds the calculated checksum, either for transmission or for comparison against the received checksum */
 	static T_UBYTE lub_rxChksum; /* Received checksum to be used to validate the integrity of the received LIN message */
 	static T_UBYTE laub_linUnifiedRxTxBuffer[8]; /* Unified data buffer. For TX this array holds the data bytes to be transmitted. For RX, this array holds the received data */
+	static T_UBYTE lub_tempSync;
+
 
 	/* IMPLEMENTATION OF THE LIN SLAVE STATE MACHINE GOES HERE... */
 	if((le_event == LINSLV_EV_BREAK_DETECTED) && (re_linslvCommState != LINSLV_IDLE)){
@@ -102,20 +104,110 @@ void linslv_slaveStateMachine(E_LINSLV_COMM_EVENTS le_event)
 
 	switch(re_linslvCommState){
 	case LINSLV_IDLE:
+		if(le_event == LINSLV_EV_BREAK_DETECTED){
+			LINSLV_HAL_DISABLE_TX_INT();
+			LINSLV_HAL_ENABLE_RX_INT();
+			LINSLV_HAL_DISABLE_BREAK_RX();
+			re_linslvCommState = LINSLV_RX_SYNCH;
+		}
 		break;
 	case LINSLV_RX_SYNCH:
+		if(le_event== LINSLV_EV_RX_OK){
+			lub_tempSync = linSlvHal_receivedByte();
+			if(lub_tempSync == LIN_SYNCH_SYMBOL){
+				re_linslvCommState = LINSLV_RX_ID;
+			}else{
+				linslv_synch_error_callback();
+				re_linslvCommState = LINSLV_IDLE;
+			}
+		}		
 		break;
 	case LINSLV_RX_ID:
-		break;
-	case LINSLV_END_OF_HEADER:
+		if(le_event== LINSLV_EV_RX_OK){
+			//rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgID = linSlvHal_receivedByte();
+			if(lincomm_checkIDparityBits(rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgID)){
+				if(rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgDir == TX){
+					LINSLV_HAL_ENABLE_TX_INT();
+					LINSLV_HAL_DISABLE_RX_INT();
+					if(rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgDataLen > 0){
+						linSlvHal_sendByte(laub_linUnifiedRxTxBuffer[0]);
+						lub_dataCounter = 1;
+						re_linslvCommState = LINSLV_TX_DATA;
+					}else {
+						lub_tempChksum = lincomm_calculateChksum(rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgID, laub_linUnifiedRxTxBuffer,rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgDataLen);
+						linSlvHal_sendByte(lub_tempChksum);
+						re_linslvCommState = LINSLV_TX_CHKSUM;
+					}
+				}else{//RX
+					LINSLV_HAL_DISABLE_TX_INT();
+					LINSLV_HAL_ENABLE_RX_INT();
+					if(rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgDataLen > 0){
+						lub_dataCounter = 1;
+						re_linslvCommState = LINSLV_RX_DATA;
+					}else {
+						re_linslvCommState = LINSLV_RX_CHKSUM;
+					}
+
+				}
+			}else{
+				/* Msg ID is incorrect. Flag the error */
+				linslv_ID_parity_error_callback();
+				re_linslvCommState = LINSLV_IDLE;
+			}
+		}	
 		break;
 	case LINSLV_TX_DATA:
+		if(le_event == LINSLV_EV_TX_OK){
+			if(rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgDataLen > lub_dataCounter){
+				linSlvHal_sendByte(laub_linUnifiedRxTxBuffer[lub_dataCounter]);
+				lub_dataCounter++;
+			}else{
+				lub_tempChksum = lincomm_calculateChksum(rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgID, laub_linUnifiedRxTxBuffer,rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgDataLen);
+				linSlvHal_sendByte(lub_tempChksum);
+				re_linslvCommState = LINSLV_TX_CHKSUM;
+			}
+		}
 		break;
 	case LINSLV_TX_CHKSUM:
+		if(le_event == LINSLV_EV_TX_OK){
+			re_linslvCommState = LINSLV_IDLE;
+			SET_LINSLV_MSG_FLAG(re_currentSlvMsg, TRUE);
+			if(rs_LINSLV_msgsStaticData[re_currentSlvMsg].MsgCallback != LIN_NULL_PTR){
+				rs_LINSLV_msgsStaticData[re_currentSlvMsg].MsgCallback ();
+			}
+			LINSLV_HAL_DISABLE_TX_INT();
+		}
 		break;
 	case LINSLV_RX_DATA:
+		if(le_event == LINSLV_EV_RX_OK){
+			laub_linUnifiedRxTxBuffer[lub_dataCounter-1] = linSlvHal_receivedByte();
+			if(rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgDataLen> lub_dataCounter){
+				lub_dataCounter++;
+			}else{
+				re_linslvCommState = LINSLV_RX_CHKSUM;
+			}
+		}
 		break;
 	case LINSLV_RX_CHKSUM:
+		if(le_event == LINSLV_EV_RX_OK){
+			lub_rxChksum = linSlvHal_receivedByte();
+			lub_tempChksum = lincomm_calculateChksum(rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgID,laub_linUnifiedRxTxBuffer,rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgDataLen);
+			if(lub_rxChksum == lub_tempChksum){
+				memcpy(rs_LINSLV_msgsStaticData[re_currentSlvMsg].MsgBuffDataStartPtr,laub_linUnifiedRxTxBuffer,rs_LINSLV_msgsStaticData[re_currentSlvMsg].msgDataLen);
+				SET_LINSLV_MSG_FLAG(re_currentSlvMsg, TRUE);
+				if(rs_LINSLV_msgsStaticData[re_currentSlvMsg].MsgCallback != LIN_NULL_PTR)
+				{
+					rs_LINSLV_msgsStaticData[re_currentSlvMsg].MsgCallback();
+				}
+			}else{
+				linslv_checksum_error_callback();
+			}
+			re_linslvCommState = LINSLV_IDLE;
+			LINSLV_HAL_DISABLE_RX_INT();
+
+		}
+		break;
+	default:
 		break;
 	}
 }
